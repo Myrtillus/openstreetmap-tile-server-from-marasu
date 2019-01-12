f45d01cf1ccaed0eeb2c57e401546d3f00f0cb46
Markus Suomi <markus.r.suomi@gmail.com>
2019-01-12 13:03:59 +0200

    Kubernetes init restart fix

--- run.sh
+++ run.sh
@@ -13,6 +13,7 @@ fi
 
 if [ "$1" = "import" ]; then
     # Kubernetes emptydir-volume hack
+    rm -rf /var/lib/postgresql/10/main
     mv /var/lib/postgresql/10/main2/* /var/lib/postgresql/10/main/
     chown -R postgres:postgres /var/lib/postgresql/10/main
     chmod 700 /var/lib/postgresql/10/main
